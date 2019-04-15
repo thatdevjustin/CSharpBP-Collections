4a66001fc05ed81b7a13b86f25542c83745491b1
Justin <justin@thatdevjustin.com>
2019-04-14 22:07:27 -0400

    using lambda experssion

--- AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -116,14 +116,18 @@ namespace Acme.Biz.Tests
             //                  where v.CompanyName.Contains("Toy")
             //                  orderby v.CompanyName
             //                  select v;
-            var vendorQuery = vendors.Where(FilterCompanies)
-                                     .OrderBy(OrderCompaniesByName);
+
+            //var vendorQuery = vendors.Where(FilterCompanies)
+            //                         .OrderBy(OrderCompaniesByName);
+
+            var vendorQuery = vendors.Where(v => v.CompanyName.Contains("Toy"))
+                                     .OrderBy(v => v.CompanyName);
 
             //Assert
             CollectionAssert.AreEqual(expected, vendorQuery.ToList());
         }
 
-        private bool FilterCompanies(Vendor v) => v.CompanyName.Contains("Toy");
-        private string OrderCompaniesByName(Vendor v) => v.CompanyName;
+        //private bool FilterCompanies(Vendor v) => v.CompanyName.Contains("Toy");
+        //private string OrderCompaniesByName(Vendor v) => v.CompanyName;
     }
 }
